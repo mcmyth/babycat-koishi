d887ce7fcf66dd77d23ecc6df23e4852c4aea22c
MC_Myth <1440126177@qq.com>
2021-02-08 18:56:46 +0800

    :bug: fix logic error

--- src/plugin/messenger.ts
+++ src/plugin/messenger.ts
@@ -7,7 +7,7 @@ export const Messenger = async (app: App): Promise<void> => {
     if (session.messageType !== 'group' || session.message == null) return
     // 遍历数组所有群号,转发到除自身外的所有群
     for (let i = 0; i < groupList.length; i++) {
-      if (groupList[i] !== session.groupId) {
+      if (groupList[i] !== session.groupId && groupList.indexOf(Number(session.groupId)) !== -1) {
         // 显示群名
         // app.bots[env.app.selfId].getGroupInfo(Number(session.groupId)).then(groupInfo => {
         //   const msg = `[${groupInfo.groupName}]\n[${session.$username}] ${session.message}`
