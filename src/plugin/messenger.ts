37c31c5f0ebca1af780d67595a3c3065ae094341
MC_Myth <1440126177@qq.com>
2021-02-10 21:18:39 +0800

    :sparkles: add authority verify

--- src/plugin/messenger.ts
+++ src/plugin/messenger.ts
@@ -1,10 +1,14 @@
 import { env } from '../config/env'
-import { App } from 'koishi-core'
+import { App, User } from 'koishi-core'
 const groupList: Array<number> = env.messenger
 export const Messenger = async (app: App): Promise<void> => {
-  app.on('message', session => {
+  app.on('message', async session => {
+    if (session.userId == null) return
+    // 判断该用户已被忽略则不处理消息转发
+    const user = await app.database.getUser(session.userId)
+    if (user.flag === User.Flag.ignore) return
     // 如果不是群消息或消息为空则不处理
-    if (session.messageType !== 'group' || session.message == null) return
+    if (user.flag) { if (session.messageType !== 'group' || session.message == null) return }
     // 遍历数组所有群号,转发到除自身外的所有群
     for (let i = 0; i < groupList.length; i++) {
       if (groupList[i] !== session.groupId && groupList.indexOf(Number(session.groupId)) !== -1) {
@@ -14,7 +18,7 @@ export const Messenger = async (app: App): Promise<void> => {
         //   app.bots[env.app.selfId].sendGroupMsg(groupList[i], msg)
         // })
         const msg = `[${session.$username}] ${session.message}`
-        app.bots[env.app.selfId].sendGroupMsg(groupList[i], msg)
+        await app.bots[env.app.selfId].sendGroupMsg(groupList[i], msg)
       }
     }
   })
