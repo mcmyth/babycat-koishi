9a2adb9ebd032a93669feda8792d10d31c2e94e3
MC_Myth <1440126177@qq.com>
2021-02-11 20:41:03 +0800

    :sparkles: add specify group for echo command

--- src/lib/commander.ts
+++ src/lib/commander.ts
@@ -1,6 +1,7 @@
 import { App } from 'koishi-core'
 import { WikiPic } from './wikipic'
 import { Authority } from './authority'
+import { env } from '../config/env'
 
 export class commander {
   static parser (text: string) {
@@ -9,9 +10,16 @@ export class commander {
   }
 
   static index (app: App) {
-    app.command('echo <message>')
-      .action((_, message) => {
-        return message
+    app.command('echo <message> [group]')
+      .action(async (_, message, group) => {
+        const _group: number = Number(group)
+        if (typeof group !== 'undefined') {
+          let _msg: string = ''
+          await app.bots[env.app.selfId].sendGroupMsg(_group, message).catch(() => { _msg = '消息发送失败,该群可能不存在' })
+          return _msg
+        } else {
+          return message
+        }
       })
 
     app.command('now').action((_) => new Date().toLocaleString())
